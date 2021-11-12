93271abb6887067d41d7ecf1df6575968508d071
rinanasir <rinanasir25@gmail.com>
2021-11-12 14:55:54 +0600

    navigation

---
+++ src/Pages/Home/BannerHome/BannerHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerHome = () => {
+    return (
+        <div>
+            <h2>This is Banner</h2>
+        </div>
+    );
+};
+
+export default BannerHome;
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import BannerHome from '../BannerHome/BannerHome';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <BannerHome></BannerHome>
+        </div>
+    );
+};
+
+export default Home;
