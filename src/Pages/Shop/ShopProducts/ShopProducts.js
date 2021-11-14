8c3d4d892a77e84a81c351007d21c74b3ca28266
rinanasir <rinanasir25@gmail.com>
2021-11-14 13:05:47 +0600

    updatedProductSource

--- src/Pages/Shop/ShopProducts/ShopProducts.js
+++ src/Pages/Shop/ShopProducts/ShopProducts.js
@@ -9,7 +9,7 @@ const ShopProducts = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('/products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
