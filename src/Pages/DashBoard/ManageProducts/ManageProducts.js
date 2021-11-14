c64fb6977a6d004b5b0a2d49a86f4b6c36d2979b
rinanasir <rinanasir25@gmail.com>
2021-11-15 01:32:03 +0600

    ManageProducts

--- src/Pages/DashBoard/ManageProducts/ManageProducts.js
+++ src/Pages/DashBoard/ManageProducts/ManageProducts.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 const ManageProducts = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const handleDelete = id => {
+        const url = `http://localhost:5000/products/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (data.deletedCount) {
+                    alert('Product Successfully Deleted');
+                    const remaining = products.filter(product => product._id !== id);
+                    setProducts(remaining);
+                }
+            });
+    };
+
     return (
-        <div>
-            <h2>manage products</h2>
-        </div>
+        <Box>
+            <Typography sx={{ mt: 4, fontWeight: 'bold' }} variant="h4">Manage Products</Typography>
+            {
+                products.map(product =>
+
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={6}>
+                            <Typography sx={{ fontWeight: 'bold', fontSize: 20 }} variant="subtitle1" color="text.secondary">
+                                {product.name}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <Button onClick={() => handleDelete(product._id)}>Delete</Button>
+                        </Grid>
+                    </Grid>
+                )
+            }
+        </Box>
     );
 };
 
--- src/Pages/ProductDetails/ProductDetails.js
+++ src/Pages/ProductDetails/ProductDetails.js
@@ -1,11 +1,9 @@
 import { faCartPlus, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Container, Grid, Typography } from '@mui/material';
+import { Button, Container, Divider, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import Rating from '@mui/material/Rating';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
 
 const ProductDetails = () => {
@@ -25,24 +23,26 @@ const ProductDetails = () => {
         <Box>
             <Navigation />
             <Container>
-                <Grid container spacing={2}>
-                    <Grid item sx={{ mt: 5 }} xs={12} md={6}>
-                        <img style={{ width: '80%' }} src={product.image} alt="" />
+                <Grid container spacing={4}>
+                    <Grid item sx={{ mt: 10 }} xs={12} md={4}>
+                        <img style={{ width: '100%' }} src={product.image} alt="" />
                     </Grid>
-                    <Grid item sx={{ textAlign: 'left', mt: 5 }} xs={12} md={6}>
+                    <Grid item sx={{ textAlign: 'left', mt: 10 }} xs={12} md={4}>
                         <Typography variant="h5">
                             {product.about}
                         </Typography>
-                        <Typography sx={{ fontWeight: 'bold', color: 'red' }} variant="h6" component="div">
+                        <Typography sx={{ fontWeight: 'bold', color: 'red', mt: 5 }} variant="h6" component="div">
                             {dollarIcon} {product.price}
                         </Typography>
-                        <Rating name="read-only" value={product.value} readOnly />
                         <br />
-                        <Button sx={{ width: '40%', mt: 5 }} style={{ backgroundColor: '#BD9200', color: 'white', fontSize: 15, fontWeight: 'bold' }} variant="contained">{cartIcon}_Add to Cart</Button>
+                        <Button sx={{ width: '50%', mt: 5 }} style={{ backgroundColor: '#BD9200', color: 'white', fontSize: 15, fontWeight: 'bold' }} variant="contained">{cartIcon}_Add to Cart</Button>
+                    </Grid>
+                    <Grid item sx={{ mt: 10 }} xs={12} md={4}>
+                        <Typography style={{ fontWeight: 'bold', color: '#BD9200' }} variant="h4">Cart</Typography>
+                        <Divider></Divider>
                     </Grid>
                 </Grid>
             </Container>
-            <Footer />
         </Box>
     );
 };
