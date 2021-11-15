901a10be59d56f15621f39afc6422b207fe60439
rinanasir <rinanasir25@gmail.com>
2021-11-15 20:18:40 +0600

    myOrders

--- src/Pages/DashBoard/MyOrders/MyOrders.js
+++ src/Pages/DashBoard/MyOrders/MyOrders.js
@@ -1,13 +1,53 @@
-import { Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import Paper from '@mui/material/Paper';
+import useAuth from '../../../hooks/useAuth';
 
 const MyOrders = () => {
+    const { user } = useAuth();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/orders?email=${user.email}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, []);
+    console.log(orders);
+
     return (
         <Box container>
-            <Typography sx={{ fontWeight: 'bold', fontSize: 20 }} variant="subtitle1" color="text.secondary">
+            <Typography sx={{ color: '#BD9200', fontWeight: 'bold' }} variant="h4" color="text.secondary">
                 My Orders
             </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="Appointments table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Customer Name</TableCell>
+                            <TableCell align="right">Location</TableCell>
+                            <TableCell align="right">City</TableCell>
+                            <TableCell align="right">Phone Number</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {orders.map((row) => (
+                            <TableRow
+                                key={row._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.userName}
+                                </TableCell>
+                                <TableCell align="right">{row.location}</TableCell>
+                                <TableCell align="right">{row.City}</TableCell>
+                                <TableCell align="right">{row.phone}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Box>
     );
 };
